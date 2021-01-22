7a2fd46326c378e94f9d81fad10afdcb7e955e22
Vetlix <31412314+Vetlix@users.noreply.github.com>
2021-01-22 23:26:56 +0100

    feat: rewrite start

---
+++ services/bot/src/Config.ts
@@ -0,0 +1,8 @@
+
+export const POSTGRES_USERNAME = '';
+export const POSTGRES_PASSWORD = '';
+export const POSTGRES_DATABASE = '';
+export const POSTGRES_PORT = 5432;
+export const POSTGRES_HOST = 'postgres';
+
+export const DISCORD_TOKEN = '';

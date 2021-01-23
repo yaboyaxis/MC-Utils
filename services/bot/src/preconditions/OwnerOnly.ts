71c71035e77ed648adc4e5ecf1af79650303dd23
Vetlix <31412314+Vetlix@users.noreply.github.com>
2021-01-23 11:25:52 +0100

    refactor(handler): minify owneronly precondition

--- services/bot/src/preconditions/OwnerOnly.ts
+++ services/bot/src/preconditions/OwnerOnly.ts
@@ -4,8 +4,6 @@ import { OWNERS } from "../BotConfig";
 
 export default class OwnerOnly extends Precondition {
   public run(message: Message) {
-    if (!OWNERS.includes(message.author.id))
-      return this.error(this.name, "Author is not registered as bot owner.");
-    return this.ok();
+    return OWNERS.includes(message.author.id) ? this.ok() : this.error(this.name, "Author is not registered as bot owner.");
   }
 }

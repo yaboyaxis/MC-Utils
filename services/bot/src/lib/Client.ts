c427d58afbf8ca281da8c1aadee375df56ddc571
Vetlix <31412314+Vetlix@users.noreply.github.com>
2021-01-31 15:38:17 +0100

    feat: possible the worst category system ever invented

--- services/bot/src/lib/Client.ts
+++ services/bot/src/lib/Client.ts
@@ -4,11 +4,21 @@ import { ClientOptions } from "discord.js";
 import Database from "./database/Database";
 import Util from "../util/Util";
 import { Logger } from "@ayanaware/logger";
+import { parse, sep } from "path";
 import "../util/Logger";
 
 const log = Logger.get("Client");
 
-Store.defaultStrategy.onLoad = (store: Store<any>, piece: Piece) => Logger.get(store.name).debug(`Loaded ${piece.name}.`);
+Store.defaultStrategy.onLoad = (store: Store<any>, piece: Piece) => {
+  Logger.get(store.name).debug(`Loaded ${piece.name}.`);
+
+  const category = parse(piece.path).dir.split(sep).pop().toLowerCase();
+  piece.category = category;
+  
+  if (!store.categories) store.categories = [];
+  if (!store.categories.includes(category)) store.categories.push(category);
+}
+
 Store.defaultStrategy.onLoadAll = (store: Store<any>) => Logger.get(store.name).info(`Loaded a total of ${store.size} ${store.name}.`);
 
 export default class Client extends SapphireClient {
@@ -17,6 +27,8 @@ export default class Client extends SapphireClient {
 
   constructor(options?: SapphireClientOptions & ClientOptions) {
     super(options);
+
+    this.commands.categories = [];
   }
 
   public async login(token: string) {
